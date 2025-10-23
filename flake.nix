3ec1d9d71b4b02cc0a091652bc546d9094e34fd9
kingro27 <kumarvasu2006@gmail.com>
2025-10-24 00:36:04 +0545

    correct

---
+++ flake.nix
@@ -0,0 +1,30 @@
+# /etc/nixos/flake.nix
+{
+  description = "New NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs@{ self, nixpkgs, home-manager, ... }: {
+    nixosConfigurations = {
+      TARDIS = nixpkgs.lib.nixosSystem {
+        modules = [
+	  ./configuration.nix
+
+	  home-manager.nixosModules.home-manager
+	  {
+	    home-manager.useGlobalPkgs = true;
+	    home-manager.useUserPackages = true;
+	    home-manager.users.doctor = import ./home.nix;
+	  }
+	];
+      };
+    };
+  };
+}

4a115aa0ce47a61eb7239155db0af45b2c453dd2
kingro27 <kumarvasu2006@gmail.com>
2025-10-24 07:41:58 +0545

    added bluetooth

--- configuration.nix
+++ configuration.nix
@@ -1,12 +1,8 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [
       ./hardware-configuration.nix
     ];
 
@@ -18,11 +14,6 @@
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
   networking.hostName = "TARDIS"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
   networking.networkmanager.enable = true;
@@ -46,6 +37,8 @@
     layout = "us";
     variant = "";
   };
+
+  hardware.bluetooth.enable = true;
 
   programs.dconf = {
     enable = true;
